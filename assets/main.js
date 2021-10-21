763f626e822cd9cda37b778c09c098de0493b5f6
Andretreccia <andreabtreccia@gmail.com>
2021-10-21 18:27:34 +0200

    problema colore casella bomba

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,20 @@
-///////////Functions
-// funzione per selezionare il livello
-
+///////////Functions //////////////////////////////////////////////
 
 //Funzione per fenerare bombe random
 function bombGenerator(cellsNumber) {
     //creare un array che dovra contenere le bombe
-    let bombs = []
+    let bombette = []
     //creare un ciclo della durata di 16 (bombe che dovranno essere inserite)
-    while (bombs.length < 16) {
+    while (bombette.length < 16) {
         const randomNumber = Math.floor(Math.random() * (16 - 1 + 1)) + 1;
         //se il numero generato è gia incluso con .includes()
-        if (!bombs.includes(randomNumber)) {
-            bombs.push(randomNumber)
-            console.log(bombs)
+        if (!bombette.includes(randomNumber)) {
+            bombette.push(randomNumber)
+           //console.log(bombette)
         }
 
     }
+    return bombette
 }
 
 //Funzione per creare celle
@@ -42,6 +41,22 @@ function createCellGrid(container, cellsNumber, cellsInline) {
         })
     }
 }
+
+function bombVerify(cellNumber, bombette) {
+    if (bombette.includes(cellNumber)) {
+        console.log("Sei esploso");
+        let cell = document.getElementsByClassName('cell')
+        cell.addEventListener("click", function () {
+            this.style.backgroundColor = "red"
+        })
+    } else {
+        console.log("numero");
+    }
+}
+
+////////////////////////////////////////////////////////////////
+
+
 
 //costante per selezionare il bottone nella DOM
 const BtnElement = document.getElementById("choi_btn")
@@ -70,8 +85,31 @@ BtnElement.addEventListener('click', function () {
 
     createCellGrid(container, cellsNumber, cellsInline)
 
-    bombGenerator(cellsNumber)
+    let bombette = bombGenerator(cellsNumber)
+    console.log(bombette)
+
+    // Select all cells
+    const cellForBomb = document.getElementsByClassName('cell');
+    //console.log((cellForBomb));
+
+    // ciclare tra le celle
+    for (let i = 0; i < cellForBomb.length; i++) {
+        const cellForBombClick = cellForBomb[i];
+        //console.log(element)
+        cellForBombClick.addEventListener('click', function () {
+            // prendere il contenuto della cell
+            const cellForBomb = parseInt(this.innerText)
+            //console.log(cellForBomb);
+            // verifica se la cella é una bomba
+            bombVerify(cellForBomb, bombette)
+        })
+
+    }
+
 })
+
+
+
 
 
 
